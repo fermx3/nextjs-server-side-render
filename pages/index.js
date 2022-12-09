ec582e457bdfd71e34eec755cc01efc4688b0bc4
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2022-12-09 09:29:32 -0600

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+const HomePage = ({ products }) => {
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        { id: "p1", title: "Product 1" },
+        { id: "p2", title: "Product 2" },
+        { id: "p3", title: "Product 3" },
+      ],
+    },
+  };
+}
+
+export default HomePage;

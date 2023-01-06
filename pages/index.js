68b0bb645e20b9f7a510593cdda4e0ea119e8669
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2023-01-06 14:56:58 -0600

    Add revalidate key to re-render on production every 60sec

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ const HomePage = ({ products }) => {
 };
 
 export async function getStaticProps() {
+  console.log("(Re-Generating...)");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
@@ -20,6 +21,7 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
+    revalidate: 60,
   };
 }
 

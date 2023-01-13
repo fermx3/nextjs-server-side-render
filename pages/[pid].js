a8b9d0a4d76049484ee17ff7ee4cc9194127245a
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2023-01-12 18:08:21 -0600

    Add getStaticPaths for pre-render dinamic pages

---
+++ pages/[pid].js
@@ -0,0 +1,61 @@
+import path from "path";
+import fs from "fs/promises";
+
+import { Fragment } from "react";
+
+const ProductDetailPage = ({ loadedProduct }) => {
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
+  const { title, description } = loadedProduct;
+
+  return (
+    <Fragment>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </Fragment>
+  );
+};
+
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const data = await getData();
+
+  const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
+  return {
+    paths: pathsWithParams,
+    fallback: true,
+  };
+}
+
+export default ProductDetailPage;
